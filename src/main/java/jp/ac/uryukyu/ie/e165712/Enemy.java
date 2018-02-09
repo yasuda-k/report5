0840aca65e9673bfd0a482388eb4efbcbf1d544e
Keisuke Yasuda <e165712@ie.u-ryukyu.ac.jp>
2018-02-09 22:38:24 +0900

    2nd

--- src/main/java/jp/ac/uryukyu/ie/e165712/Enemy.java
+++ src/main/java/jp/ac/uryukyu/ie/e165712/Enemy.java
@@ -4,12 +4,32 @@ public class Enemy extends LivingThing{
     int hitPoint;
     String name;
     boolean dead;
+    int attack;
 
     public Enemy (String name, int maximumHP, int attack) {
         super(name, maximumHP, attack);
         this.hitPoint = super.getHitPoint();
         this.name = super.getName();
         this.dead = super.isDead();
+        this.attack = super.getAttack();
+    }
+
+    public void attack(LivingThing opponent) {
+        if (isDead() == false) { //report3の時の修正
+            int damage = (int) (Math.random() * attack);
+
+            if  (damage != 0) {
+                if ((int) (Math.random() * 100) <= 30) {
+                    damage *= 2;
+                    System.out.printf("%sの攻撃！痛恨の一撃！！%sに%dのダメージを与えた！！\n", name, opponent.getName(), damage);
+                } else {
+                    System.out.printf("%sの攻撃！%sに%dのダメージを与えた！！\n", name, opponent.getName(), damage);
+                }
+            }else {
+                System.out.printf("%sの攻撃！,,,だが、%sは攻撃を回避した！\n", name, opponent.getName());
+            }
+            opponent.wounded(damage);
+        }
     }
 
     /**
@@ -20,7 +40,7 @@ public class Enemy extends LivingThing{
     public void wounded(int damage){
         hitPoint -= damage;
         if( hitPoint < 0 ) {
-            super.setDead(true);
+            setDead(true);
             System.out.printf("%sは倒れた。\n", name);
         }
     }
--- src/main/java/jp/ac/uryukyu/ie/e165712/Hero.java
+++ src/main/java/jp/ac/uryukyu/ie/e165712/Hero.java
@@ -4,12 +4,32 @@ public class Hero extends LivingThing {
     int hitPoint;
     String name;
     boolean dead;
+    int attack;
 
     public Hero(String name, int maximumHP, int attack) {
         super(name, maximumHP, attack);
         this.hitPoint = super.getHitPoint();
         this.name = super.getName();
         this.dead = super.getDead();
+        this.attack = super.getAttack();
+    }
+
+    public void attack(LivingThing opponent) {
+        if (isDead() == false) { //report3の時の修正
+            int damage = (int) (Math.random() * attack);//ダメージ決定
+
+            if  (damage != 0) {
+                if ((int) (Math.random() * 100) <= 40) {
+                    damage *= 2;
+                    System.out.printf("%sの攻撃！会心の一撃！！%sに%dのダメージを与えた！！\n", name, opponent.getName(), damage);
+                } else {
+                    System.out.printf("%sの攻撃！%sに%dのダメージを与えた！！\n", name, opponent.getName(), damage);
+                }
+            }else {
+                System.out.printf("%sの攻撃！,,,だが、%sは攻撃を回避した！\n", name, opponent.getName());
+            }
+            opponent.wounded(damage);
+        }
     }
     /**
      * 自身へ攻撃されたときのダメージ処理をするメソッド。
@@ -19,8 +39,8 @@ public class Hero extends LivingThing {
     public void wounded(int damage){
         hitPoint -= damage;
         if( hitPoint < 0 ) {
-            setDead(dead);
-            System.out.printf("勇者%sは道半ばで力尽きてしまった。\n", name);
+            setDead(true);
+            System.out.printf("%sは道半ばで力尽きてしまった。\n", name);
         }
     }
 }
